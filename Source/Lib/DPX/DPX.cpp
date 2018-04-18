bcf027e47390df5f112fae3bd29fdb02f46c4c35
Kieran O'Leary <kieran.o.leary@gmail.com>
2018-04-18 09:08:06 +0100

    DPX 12-bit packed enabled

--- Source/Lib/DPX/DPX.cpp
+++ Source/Lib/DPX/DPX.cpp
@@ -50,7 +50,7 @@ struct dpx_tested
     dpx::style                  Style;
 };
 
-const size_t DPX_Tested_Size = 24;
+const size_t DPX_Tested_Size = 25;
 struct dpx_tested DPX_Tested[DPX_Tested_Size] =
 {
     { RGB       ,  8, Packed , BE, dpx::RGB_8 },
@@ -59,7 +59,7 @@ struct dpx_tested DPX_Tested[DPX_Tested_Size] =
     { RGB       ,  8, MethodA, LE, dpx::RGB_8 },
     { RGB       , 10, MethodA, LE, dpx::RGB_10_FilledA_LE },
     { RGB       , 10, MethodA, BE, dpx::RGB_10_FilledA_BE },
-//    { RGB       , 12, Packed , BE, dpx::RGB_12_Packed_BE }, // Not supported by FFmpeg DPX parser
+    { RGB       , 12, Packed , BE, dpx::RGB_12_Packed_BE }
     { RGB       , 12, MethodA, BE, dpx::RGB_12_FilledA_BE },
     { RGB       , 12, MethodA, LE, dpx::RGB_12_FilledA_LE },
     { RGB       , 16, Packed , BE, dpx::RGB_16_BE },
